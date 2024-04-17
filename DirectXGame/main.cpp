d3c90f1d8d817a30f616cf511eb60c124c90a1f1
furutaayumu <k023g0086@g.neec.ac.jp>
2024-04-17 14:04:16 +0900

    aaaaa

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,10 +8,6 @@
 #include "WinApp.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
-<<<<<<< HEAD
-//テスト
-=======
->>>>>>> ec00eb92037cafd0a932cc9eb6abcb66344ec854
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WinApp* win = nullptr;
 	DirectXCommon* dxCommon = nullptr;
